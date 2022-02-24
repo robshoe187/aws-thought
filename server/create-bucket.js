7c56421ca70966b8ef8908dbde7f12363aab16fa
Rob Shoemaker <rob.shoe187@gmail.com>
2022-02-24 18:29:23 -0500

    added create-bucket.js

---
+++ server/create-bucket.js
@@ -0,0 +1,22 @@
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+//set region
+AWS.config.update({region: 'us-east-1'});
+
+// Create S3 service object
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// Create the parameters for calling createBucket
+var bucketParams = {
+    Bucket : "user-images-" + uuidv4()
+  };
+
+  // call S3 to create the bucket
+s3.createBucket(bucketParams, (err, data) => {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success");
+    }
+  });
