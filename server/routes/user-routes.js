2ba0ee791dfc5f2b390cad16dc41c6dd8bb10f57
Rob Shoemaker <rob.shoe187@gmail.com>
2022-02-28 10:23:20 -0500

    added routes for image display

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -11,7 +11,13 @@ const table = "Thoughts";
 
 router.get('/users', (req, res) => {
     const params = {
-      TableName: table
+      TableName: table,
+      Item: {
+        "username": req.body.username,
+        "createdAt": Date.now(),
+        "thought": req.body.thought,
+        "image": req.body.image
+      }
     };
     // Scan return all items in the table
   dynamodb.scan(params, (err, data) => {
@@ -31,12 +37,13 @@ router.get('/users/:username', (req, res) => {
     ExpressionAttributeNames: {
       "#un": "username",
       "#ca": "createdAt",
-      "#th": "thought"
+      "#th": "thought",
+      "#img": "image"
     },
     ExpressionAttributeValues: {
       ":user": req.params.username
     },
-    ProjectionExpression: "#th, #ca",
+    ProjectionExpression: "#un, #th, #ca, #img",
     ScanIndexForward: false
   };
   
@@ -58,7 +65,8 @@ router.post('/users', (req, res) => {
     Item: {
       "username": req.body.username,
       "createdAt": Date.now(),
-      "thought": req.body.thought
+      "thought": req.body.thought,
+      "image": req.body.image
     }
   };
   // database call
--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -7,7 +7,8 @@ const params = fileName => {
     const imageParams = {
       Bucket: 'user-images-f0a21a01-8661-4cf4-b8eb-2592c38a0906',
       Key: `${uuidv4()}.${fileType}`,
-      Body: fileName.buffer
+      Body: fileName.buffer,
+      ACL: 'public-read'
     };
   
     return imageParams;
